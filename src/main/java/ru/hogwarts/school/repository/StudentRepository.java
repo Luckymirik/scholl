125b29be456e4ecc05e1d1cde427318aa27b28b2
LuckyMirik <Luckymirik@mail.ru>
2023-09-29 12:51:32 +0300

    Закончил ДЗ

--- src/main/java/ru/hogwarts/school/repository/StudentRepository.java
+++ src/main/java/ru/hogwarts/school/repository/StudentRepository.java
@@ -1,8 +1,8 @@
 package ru.hogwarts.school.repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
+
 
 import java.util.List;
 
